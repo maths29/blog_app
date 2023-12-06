9635f0826d7ba3dbe78a7d2fa5fe119a8ba291b6
mathsoft29 <iwuconcept@gmail.com>
2023-12-06 17:48:40 +0100

    Add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,13 @@
 class PostsController < ApplicationController
-  def index; 
-      @user = User.find(params[:user_id])
-      @posts = @user.posts 
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
+  def show
+    logger.debug(params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 end
-
-  def show; 
-  logger.debug(params)
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:id])
-      @comments = @post.comments
-
-   end
-   
- end
