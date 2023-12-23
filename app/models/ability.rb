76ff15f2f7a9981e6cd0b8534120ed5eaea551de
mathsoft29 <iwuconcept@gmail.com>
2023-12-23 08:54:15 +0100

    Add the cancan authorization ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,2 +1,19 @@
 # frozen_string_literal: true
+class Ability
+  include CanCan::Ability
 
+  def initialize(user)
+    user ||= User.new # Guest user (not logged in)
+  
+      # Define abilities for different roles
+      if user.admin?
+        can :manage, :all # Admins can manage all resources
+      else
+        can :read, :all
+        can :create, Post
+        can :destroy, Post, author_id: user.id
+        can :destroy, Comment, user_id: user.id
+  
+      end
+    end
+end
