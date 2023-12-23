f04983c0d1bd2a5b871686393fbe84529b2b9c44
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-23 10:21:50 +0200

    Add admin method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  
+  ROLES = %w[admin user].freeze
+
+  def admin?
+    role == 'admin'
+  end
+
   
 end
