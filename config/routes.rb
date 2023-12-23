526f17538f7ae65415eb08e544553c14e8775754
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-23 22:59:40 +0200

    Add routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "users#index"
- 
+  resources :users do
+    resources :posts do
+      member do
+        get 'api_index'
+        get 'api_comments'
+        post 'api_add_comment'
+      end
+    end
+  end
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy ] do
       resources :comments, only: [:create, :new, :destroy]
