57817662d1cb1e8caf544230b200cc41f9b186dc
mathsoft29 <iwuconcept@gmail.com>
2023-12-23 08:51:39 +0100

    Implement the authorization structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,5 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   
-  ROLES = %w[admin user].freeze
-
-  def admin?
-    role == 'admin'
-  end
-
-  def self.recent_posts(user)
-    user.posts.order(created_at: :desc).limit(3)
-  end
+  
 end
