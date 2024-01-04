33855f7d4fd73ade82d1f33a1be0831a20b09297
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-04 18:12:32 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,6 @@ Rails.application.routes.draw do
 
 
   root "users#index"
-  resources :users do
-    resources :posts do
-      member do
-        get 'api_index'
-        get 'api_comments'
-        post 'api_add_comment'
-      end
-    end
-  end
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy ] do
       resources :comments, only: [:create, :new, :destroy]
