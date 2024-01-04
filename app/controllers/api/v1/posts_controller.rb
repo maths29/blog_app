fea33b31b7ca077518f56e00ffb521c5c1a390d5
mathsoft29 <iwuconcept@gmail.com>
2024-01-04 13:07:49 +0100

    create api for user posts

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::PostsController < ApplicationController
+    
+      def index
+        @user = User.find(params[:id])
+        @posts = @user.posts.all
+        render json: @posts
+      end
+end
+  
