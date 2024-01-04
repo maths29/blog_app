8c0cf34da169800a2cf1b833b0a9e6c44b2a8eea
mathsoft29 <iwuconcept@gmail.com>
2024-01-04 13:08:45 +0100

    Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:index, :create, :new, :destroy]
+          resources :likes, only: [:create, :new]
+        end
+      end
+    end
+  end
+
+
   root "users#index"
   resources :users do
     resources :posts do
