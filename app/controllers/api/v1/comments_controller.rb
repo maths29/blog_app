da0ddd496706cb21cb8bb610dacc5b011a43322b
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-04 18:14:45 +0200

    Update comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,26 +3,25 @@ class Api::V1::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
     render json: @comments
   end
 
   def create
+    @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
+    @comment.user = current_user
     if @comment.save
       render json: @comment, status: :created
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: { error: 'Failed to create comment' }, status: :unprocessable_entity
     end
   end
 
   private
 
-  def find_post
-    @post = Post.find(params[:post_id])
-  end
-
   def comment_params
-    params.require(:comment).permit(:text, :post_id, :user_id)
+    params.require(:comment).permit(:text, :post_id)
   end
 end
