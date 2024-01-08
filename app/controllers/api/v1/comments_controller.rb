5b5e296a2327ee294111bd2e7f64226a47a41803
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-08 23:27:18 +0200

    Update comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -21,6 +21,6 @@ class Api::V1::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.permit(:body, :post_id)
+    params.require(:comment).permit(:body, :post_id)
   end
 end
