371b123001af22505f32a6448577760337aa84ff
mathsoft29 <iwuconcept@gmail.com>
2024-01-08 13:08:47 +0100

    Allow user to create post using UI

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 class Ability
   include CanCan::Ability
+
   def initialize(user)
-    user ||= User.new # Guest user (not logged in)
-    can :read, Post
-    if user.persisted?
-      can :create, Comment
-      can :read, Post, author_id: user.id
-      can :destroy, Comment, author_id: user.id
+    user ||= User.new 
+    can :create, Comment if user.persisted? 
       if user.admin?
         can :manage, :all
       else
-        can [:update, :api_index, :api_comments, :api_add_comment], Post, author_id: user.id
+        can :read, :all
+        can :create, Post
+        can :destroy, Post, author_id: user.id
+        can :destroy, Comment, author_id: user.id
+  
       end
     end
-  end
 end
